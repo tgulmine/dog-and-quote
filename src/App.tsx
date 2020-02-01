c8e35607408bbef3b437dd4ab0a387ff8d1a52f6
tgulmine <tgulmine@gmail.com>
2020-02-01 04:08:46 -0300

    responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -29,17 +29,21 @@ const App: React.FC = () => {
     }
   }
 
+  function clickImage() {
+    if (window.innerWidth < 768) getData();
+  }
+
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <div className="bg-yellow-700 w-screen h-screen flex-col items-center">
-      <div className="text-5xl font-bold pt-4 text-center text-white">Dog and Quote</div>
+    <div className="bg-yellow-700 w-screen h-screen flex-col items-center justify-between md:justify-start">
+      <div className="text-3xl md:text-4xl xl:text-5xl font-bold pt-4 text-center text-white">Dog and Quote</div>
 
-      <div className="dog-size flex justify-center items-center mt-8">
+      <div className="dog-height flex justify-center items-center mt-8">
         {active ? (
-          <img className="dog-max-size" alt="" src={dog} />
+          <img className="dog-max-size" alt="" src={dog} onClick={clickImage} />
         ) : (
           <div className="lds-ring">
             <div></div>
@@ -50,13 +54,18 @@ const App: React.FC = () => {
         )}
       </div>
 
-      <div className="text-lg font-light mt-4 text-white text-center">{quote}</div>
       <div
-        className="border border-white rounded cursor-pointer ml-auto mr-auto text-white w-1/6 mt-20
+        className="ml-auto mr-auto text-base lg:text-lg xl:text-xl font-normal italic mt-4 text-white text-center
+      max-w-xs md:max-w-lg lg:max-w-xl"
+      >
+        {quote}
+      </div>
+      <div
+        className="btn border border-white rounded cursor-pointer ml-auto mr-auto text-white w-1/5 lg:w-1/6 xl:w-1/7 mt-10 lg:mt-12 xl:mt-16
         hover:bg-white hover:text-yellow-700"
         onClick={getData}
       >
-        <div className="text-lg text-center mt-2 mb-2 font-bold">Get a new dog</div>
+        <div className="text-sm lg:text-base xl:text-lg text-center mt-2 mb-2 font-bold">Get a new dog</div>
       </div>
     </div>
   );
