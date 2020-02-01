a540673ff3bf7856a8efe04f01abdb080f7ee998
tgulmine <tgulmine@gmail.com>
2020-02-01 05:48:49 -0300

    icon fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import './styles/main.scss';
 import axios from 'axios';
 import logo from './img/logo.png';
-import pattern from './img/pattern.png';
 
 const App: React.FC = () => {
   const [dog, setDog] = useState('');
